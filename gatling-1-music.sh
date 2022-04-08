c213b8ceb4d86cc94e7e644ce040bbdfb307531a
bucktoothsir <bucktoothsir@gmail.com>
2022-04-07 19:17:14 -0700

    files for gatling

---
+++ gatling-1-music.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+#
+# gatling-1-music.sh
+# Copyright (C) 2022 bucktoothsir <bucktoothsir@liuruishandeMacBook-Pro.local>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+#!/usr/bin/env bash
+docker container run --detach --rm \
+  -v ${PWD}/gatling/results:/opt/gatling/results \
+  -v ${PWD}/gatling:/opt/gatling/user-files \
+  -v ${PWD}/gatling/target:/opt/gatling/target \
+  -e CLUSTER_IP=`tools/getip.sh kubectl istio-system svc/istio-ingressgateway` \
+  -e USERS=1 \
+  -e SIM_NAME=ReadMusicSim \
+  --label gatling \
+  ghcr.io/scp-2021-jan-cmpt-756/gatling:3.4.2 \
+  -s proj756.ReadMusicSim
